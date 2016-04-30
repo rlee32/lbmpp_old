950d4e25087c57ec8591435acd0511f338da6570
Robert Lee <rlee32@gatech.edu>
2016-04-30 19:54:42 -0400

    test

--- driver.cc
+++ driver.cc
@@ -38,17 +38,17 @@ int main(int argc, char ** argv)
   
   std::size_t cx = sim.get_cell_count_0();
   std::size_t cy = sim.get_cell_count_1();
-  string grid_string = "G"+to_string( (int)cx );
+  string grid_string = "G"+to_string( (long long)cx );
   if( cx != cy )
   {
-    grid_string += "x"+to_string( (int)cy );
+    grid_string += "x"+to_string( (long long)cy );
   }
-  string mach_string = "M"+to_string( (int)(sim.get_M()*1000.0) );
-  string timesteps_string = "T"+to_string( (int)(sim.get_timesteps()/1000) );
-  string relax_model_string = "RM"+to_string( (int)(sim.get_relax_model()) );
-  string vc_model_string = "VCM"+to_string( (int)(sim.get_vc_model()) );
-  string nucf_string = "VCF"+to_string( (int)round(sim.get_nucf()*10.0) );
-  string re_string = "Re"+to_string( (int)round(sim.get_Re()) );
+  string mach_string = "M"+to_string( (long long)(sim.get_M()*1000.0) );
+  string timesteps_string = "T"+to_string( (long long)(sim.get_timesteps()/1000) );
+  string relax_model_string = "RM"+to_string( (long long)(sim.get_relax_model()) );
+  string vc_model_string = "VCM"+to_string( (long long)(sim.get_vc_model()) );
+  string nucf_string = "VCF"+to_string( (long long)round(sim.get_nucf()*10.0) );
+  string re_string = "Re"+to_string( (long long)round(sim.get_Re()) );
   string case_name = grid_string+"_"+mach_string+"_"+timesteps_string+"_"
     +relax_model_string+"_"+vc_model_string+"_"+nucf_string+"_"+re_string;
   
